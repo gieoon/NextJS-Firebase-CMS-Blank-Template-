41a2cf4d8674b07cece7ed13dafc1ee77ba6e0aa
gieoon <jun.a.kagaya@gmail.com>
2023-01-30 04:48:53 +1300

    Delete CMS_HTML_Field.js

--- CMS/shared/CMS_HTML_Field.js
+++
@@ -1,5 +0,0 @@
-export default function CMS_HTML_Field({id, websiteContent}) {
-    return <div id={id} className="cp-editable" dangerouslySetInnerHTML={{ 
-        __html: websiteContent[id] 
-    }} />
-}
