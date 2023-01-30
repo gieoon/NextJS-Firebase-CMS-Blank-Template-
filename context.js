084a29cb6f8fd21d4a1cf6c1eacd986d8be23564
gieoon <jun.a.kagaya@gmail.com>
2023-01-31 02:30:40 +1300

    Update context.js

--- context.js
+++ context.js
@@ -8,23 +8,12 @@ function GlobalContextProvider(props) {
     
     const [websiteContent, setWebsiteContent] = useState({});
     const [trips, setTrips] = useState([]);
-    const [activities, setActivities] = useState([]);
-    const [searchTerm, setSearchTerm] = useState([]);
-    const [destinations, setDestinations] = useState([]);
-    const [languages, setLanguages] = useState([]);
-    const [reviews, setReviews] = useState([]);
-
-    const [currentTrip, setCurrentTrip] = useState({});
 
     // Listen to changes in the DB as well, after static pre-rendering.
 
     useEffect(() => {
         
-        loadDynamicDataSnapshot(PROJECT_NAME, 'Activities', (a) => setActivities(a));
         loadDynamicDataSnapshot(PROJECT_NAME, 'Trips', (t) => setTrips(t));
-        loadDynamicDataSnapshot(PROJECT_NAME, 'Destinations', (d) => setDestinations(d));
-        loadDynamicDataSnapshot(PROJECT_NAME, 'Languages', (l) => setLanguages(l));
-        loadDynamicDataSnapshot(PROJECT_NAME, 'Reviews', (r) => setReviews(r));
         loadWebpageDataSnapshot(PROJECT_NAME, (w) => setWebsiteContent(w));
         
     }, []);
@@ -35,18 +24,6 @@ function GlobalContextProvider(props) {
             setWebsiteContent: setWebsiteContent,
             trips: trips,
             setTrips: setTrips,
-            searchTerm: searchTerm,
-            setSearchTerm: setSearchTerm,
-            destinations: destinations,
-            setDestinations: setDestinations,
-            activities: activities,
-            setActivities: setActivities,
-            languages: languages,
-            setLanguages: setLanguages,
-            reviews: reviews,
-            setReviews: setReviews,
-            currentTrip: currentTrip,
-            setCurrentTrip: setCurrentTrip,
         }}>
             {props.children}
         </GlobalContext.Provider>
