fd79bd8367643fdc3e35738bdfdc3badf84928f6
gieoon <jun.a.kagaya@gmail.com>
2023-05-19 15:52:44 +1200

    Update CMS_String_Field.js
    Add style

--- CMS/shared/CMS_String_Field.js
+++ CMS/shared/CMS_String_Field.js
@@ -2,15 +2,17 @@ import { useContext } from "react"
 import { GlobalContext } from "../../context"
 
 export default function CMS_String_Field({
-    id, 
-    placeholder,
-    c,
+    id, // Unique ID (Firestore document object key)
+    placeholder, // Default text
+    c, // Custom className
+    style, // Object
 }) {
     
     const {websiteContent} = useContext(GlobalContext);
 
     return <p 
         id={id} 
+        style={style}
         className={"cp-editable " + c}>{
             websiteContent[id] 
                 ? websiteContent[id] 
