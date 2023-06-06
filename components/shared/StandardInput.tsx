767dfd5acb95a16cedb70bcb4775ca1a236d722c
gieoon <jun.a.kagaya@gmail.com>
2023-06-07 10:06:50 +1200

    Update StandardInput.tsx

--- components/shared/StandardInput.tsx
+++ components/shared/StandardInput.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import styles from '../styles/StandardInput.module.scss';
+import styles from '../../styles/StandardInput.module.scss';
 
 interface StandardInputProps {
     label: string,
