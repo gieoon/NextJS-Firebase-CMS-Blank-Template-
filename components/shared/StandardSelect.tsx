b5d623663fd4b43abb0812735487da4e97be8611
gieoon <jun.a.kagaya@gmail.com>
2023-06-07 10:05:41 +1200

    Create StandardSelect.tsx

---
+++ components/shared/StandardSelect.tsx
@@ -0,0 +1,40 @@
+import { FC, ReactElement } from "react";
+import { ActionMeta, InputActionMeta } from "react-select";
+// import Select from "react-select/dist/declarations/src/Select";
+import Select from 'react-select';
+import styles from '../styles/StandardSelect.module.scss';
+
+interface StandardSelectProps {
+    label: ReactElement,
+    options: any[],
+    onChange: Function,
+}
+
+const StandardSelect: FC<StandardSelectProps> = ({
+    label,
+    options,
+    onChange,
+}) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <Select 
+                options={options}
+                // value={options[0]}     
+                onChange={function (newValue: any, actionMeta: ActionMeta<any>): void {
+                    onChange(newValue.value);
+                } }
+                inputValue={""}
+                onInputChange={function (newValue: string, actionMeta: InputActionMeta): void {
+                    // throw new Error("Function not implemented.");
+                } } onMenuOpen={function (): void {
+                    // throw new Error("Function not implemented.");
+                } } onMenuClose={function (): void {
+                    // throw new Error("Function not implemented.");
+                } }
+            />
+        </div>
+    );
+}
+
+export default StandardSelect;
